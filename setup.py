8bbb57cbd6a6df12bcaee291c855d651c0b75d0c
pavannagula <npavankumar36@gmail.com>
2024-06-26 11:22:40 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="RAG Chatbot System using Mistral and Haystack",
     version="0.0.1",
     author="pavannagula",
-    author_email="pavannagula76@gmail.com",
+    author_email="npavankumar36@gmail.com",
     packages=find_packages(),
     install_requires=["pinecone-haystack","haystack-ai","fastapi","uvicorn","python-dotenv","pathlib"]
 )
