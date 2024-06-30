a9832787bc45d37617c6c8136cc1b83da9d56e90
pavannagula <npavankumar36@gmail.com>
2024-06-30 01:48:09 -0400

    updated app.py

--- app.py
+++ app.py
@@ -7,43 +7,28 @@ from Chatbot_system.RetrievalandGeneration import get_result
 st.set_page_config(page_title="RAG Chatbot Using Haystack Mistral & Pinecone", layout="wide")
 st.title("RAG With Haystack Mistral & Pinecone")
 
-# Input Form
-with st.form(key="query_form"):
-    question = st.text_area("Ask Anything!", height=150)
-    submit_button = st.form_submit_button(label="Submit")
 
-# Output section for the answer
-answer_placeholder = st.empty()
-info_placeholder = st.empty()
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-# Processing the Form Submission
-if submit_button:
-    if not question:
-        st.error("Please enter your query!")
-    else:
-        info_placeholder.info("Processing your query... Please wait.")
-        try:
-            answer = get_result(question)
-            answer_placeholder.text_area("Answer", value=answer, height=150, disabled=True)
-            info_placeholder.empty()  
-        except Exception as e:
-            info_placeholder.empty()  
-            st.error(f"An error occurred: {e}")
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
-# CSS styling
-st.markdown(
-    """
-    <style>
-    .stTextArea {
-        font-family: 'Fira Code', monospace;
-        background-color: #f8f9fa !important;
-        color: #495057 !important;
-    }
-    .stButton button {
-        background-color: #007bff !important;
-        color: #ffffff !important;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+# Accept user input
+if prompt := st.chat_input("Ask me anything!"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        user_prompt = st.session_state.messages[-1]["content"]
+        
+        answer = get_result(user_prompt)
+        response = st.write(answer)
+    st.session_state.messages.append({"role": "assistant", "content": answer})
